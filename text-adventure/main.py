e1b90575fdf11d25b6fdc296a5ef7d8e2e02b531
Sir106 <github@id-e.de>
2018-02-18 08:46:22 +0100

    week2: new rooms&items

--- text-adventure/main.py
+++ text-adventure/main.py
@@ -7,11 +7,33 @@ print("")
 print("init started...")
 print("")
 kitchen = Room("Kitchen")
-kitchen.set_description("A dark and dirty room buzzing with flies.")
-print("")
-print("init finised...")
+kitchen.set_description("A dank and dirty room buzzing with flies.")
 
-kitchen.describe()
+dining_hall = Room("Dining Hall")
+dining_hall.set_description("A large room with ornate golden decorations on each wall.")
+
+ballroom = Room("Ballroom")
+ballroom.set_description("A vast room with a shiny wooden floor. Huge candlesticks guard the entrance.")
+
+print("linking rooms...")
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+print("linking finished!")
+dining_hall.show_linked_rooms()
+print("")
+print("init finished...")
+
+# dining_hall.get_details()
+
+current_room = kitchen          
+
+while True:		
+    print("\n")         
+    current_room.get_details()         
+    command = input("> ")    
+    current_room = current_room.move(command)
 
 print("-------------")
 print("<--- END --->")
--- text-adventure/room.py
+++ text-adventure/room.py
@@ -1,11 +1,15 @@
 class Room():
     def __init__(self, room_name):
         self.name = room_name
+        self.linked_rooms = {}
         self.description = "empty"
         print ("created room >"+self.name+"<")
     
     def get_description(self):
         return self.description
+
+    def get_name(self):
+        return self.name
 
     def set_description(self, room_description):    
         self.description = room_description
@@ -13,4 +17,25 @@ class Room():
     
     def describe(self):
         print(self.description)
+
+    def show_linked_rooms(self):
+        print( self.name + " linked rooms :" + repr(self.linked_rooms) )
+
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
+        print ("linked room >"+direction+"< to room >"+self.name+"<")
     
+    def get_details(self):
+        print(self.name)
+        print("------------------")
+        print(self.description)
+        for direction in self.linked_rooms:
+            room = self.linked_rooms[direction]
+            print( "The " + room.get_name() + " is " + direction)
+
+    def move(self, direction):
+        if direction in self.linked_rooms:
+            return self.linked_rooms[direction]
+        else:
+            print("You can't go that way")
+        return self
