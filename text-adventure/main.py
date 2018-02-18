5e8e6d93f5659fa6f119914f32d2fba04baa8cd1
Sir106 <github@id-e.de>
2018-02-18 07:52:17 +0100

    started with text-adventure

---
+++ text-adventure/main.py
@@ -0,0 +1,6 @@
+from room import Room
+
+kitchen = Room("Kitchen")
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+kitchen.describe()
+input("finished - Press Enter to close")
---
+++ turtle-race/my_drawing.py
@@ -0,0 +1,10 @@
+from shapes import Triangle, Rectangle, Oval
+
+rect1 = Rectangle()
+
+rect1.set_width(200)
+rect1.set_height(100)
+rect1.set_color("blue")
+
+rect1.draw()
+input("finished - Press Enter to close")
